ca69557ecda7f57876c61291cb50033256837d3a
Vansh <vanshshah880@gmail.com>
2023-01-20 13:49:17 +0530

    Added Authorization

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,24 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from '@apollo/server/standalone';
-import typeDefs from './graphql/typeDefs.js';
+import { startStandaloneServer } from "@apollo/server/standalone";
+import typeDefs from "./graphql/typeDefs.js";
 import jwt from "jsonwebtoken";
 // import resolvers from './graphql/resolvers.js';
 import dbConnect from "./config/database.js";
 import resolvers from "./graphql/resolvers.js";
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context:({req})=>{
-     const {authorization} = req.headers;
-     if (authorization) {
-        const {id} = jwt.verify(authorization,"KHGSFHJKKLBN123dgvvgtyyuujbbb")
-        return {id}
-     }
-    }
-})
-
 dbConnect();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    const { authorization } = req.headers;
+    if (authorization) {
+      const { id } = jwt.verify(authorization, "KHGSFHJKKLBN123dgvvgtyyuujbbb");
+      return { id };
+    }
+  },
+});
 
-const { url } =  await startStandaloneServer(server, {
-    listen: { port: 4000 },
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
 });
 console.log(`🚀 Server ready at ${url}`);
